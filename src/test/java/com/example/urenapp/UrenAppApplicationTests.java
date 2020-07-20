78399810b5364a1fbaad53810116734af5566fb3
CasparSteinebach <casparsteinebach@gmail.com>
2020-07-20 09:35:29 +0200

    fullstackstart

---
+++ src/test/java/com/example/urenapp/UrenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
