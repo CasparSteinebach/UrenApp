7c2f425bc715d523de8293ff3d9108670ba075d7
Mauricepns <66377105+Mauricepns@users.noreply.github.com>
2020-07-20 17:03:30 +0200

    eerste update

--- src/main/java/com/example/urenapp/Controller/UrenAppService.java
+++
@@ -1,5 +0,0 @@
-package com.example.urenapp.Controller;
-
-public class UrenAppService {
-
-}
--- src/main/java/com/example/urenapp/Model/HRManager.java
+++ src/main/java/com/example/urenapp/Model/HRManager.java
@@ -1,8 +1,14 @@
 package com.example.urenapp.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class HRManager {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) //this is the auto increment
+	long id; //primary key
+	
 }
