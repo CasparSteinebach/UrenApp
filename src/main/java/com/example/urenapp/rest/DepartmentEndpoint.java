4d869eede1ed873665e77105900770bfa5bc74b2
ifd459 <isabella.f.deane@gmail.com>
2020-07-24 13:53:34 +0200

    1c

--- src/main/java/com/example/urenapp/rest/DepartmentEndpoint.java
+++ src/main/java/com/example/urenapp/rest/DepartmentEndpoint.java
@@ -2,10 +2,13 @@ package com.example.urenapp.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.urenapp.Controller.DepartmentService;
+import com.example.urenapp.Controller.EmployeeService;
 import com.example.urenapp.Model.Department;
 
 @RestController
@@ -14,7 +17,10 @@ public class DepartmentEndpoint {
 
 	@Autowired 
 	DepartmentService uas;
-
+	@Autowired
+	EmployeeService employeeservice;
+	
+	
 	@GetMapping("/loonsverhoging/{id}")
 	public double NieuwSalaris(double salaris) {
 		return salaris;
@@ -23,5 +29,10 @@ public class DepartmentEndpoint {
 	@GetMapping("/all") 
 	public Iterable<Department> getDepartment() {
 		return uas.getAllDepartments();
+	}
+	
+	@PutMapping("/addemployeetodepartment/{departmentid}/{employeeid}")
+	public Department addEmployeeToDepartment(@PathVariable(value = "departmentid") long departmentid,@PathVariable(value = "employeeid") long employeeid) {
+		return uas.addEmployee(departmentid, employeeid);
 	}
 }	
