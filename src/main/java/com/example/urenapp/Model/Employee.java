2deb75b5fde06a7dbeb5f21b5b3787231b49ef7a
Mauricepns <66377105+Mauricepns@users.noreply.github.com>
2020-07-24 16:27:41 +0200

    foutje

--- src/main/java/com/example/urenapp/Model/Employee.java
+++ src/main/java/com/example/urenapp/Model/Employee.java
@@ -13,7 +13,7 @@ public class Employee {
 	long id; //primary key
 	String name;
 	double salary;
-	ArrayList <Message> messages;
+//	ArrayList <Messages> messages;
 	public long getId() {
 		return id;
 	}
@@ -32,10 +32,10 @@ public class Employee {
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
-//	public ArrayList<Message> getMessages() {
+//	public ArrayList<Messages> getMessages() {
 //		return messages;
 //	}
-//	public void setMessages(ArrayList<Message> messages) {
+//	public void setMessages(ArrayList<Messages> messages) {
 //		this.messages = messages;
 //	}
 	
